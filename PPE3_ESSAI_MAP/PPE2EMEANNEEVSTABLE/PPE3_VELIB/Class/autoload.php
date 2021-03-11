5806f4bc982581ca9db4b720e73afb06fd7cee1e
nicolas-vukelic <73752027+nicolas-vukelic@users.noreply.github.com>
2021-03-11 22:01:03 +0100

    Delete PPE3_ESSAI_MAP directory

--- PPE3_ESSAI_MAP/PPE2EMEANNEEVSTABLE/PPE3_VELIB/Class/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-/******************************fonction autoload **********************************************/
-/* permet de charger automatiquement les fichiers presents dans ce dossier */
-function __autoload($nomClasse) {
-	require_once $nomClasse.'.class.php';
-}
--- PPE3_ESSAI_MAP/PPE2EMEANNEEVSTABLE/PPE3_VELIB/INDEX.php
+++
@@ -1,4 +0,0 @@
-<?php
-// redirection vers la page index dans la VUE
-header ( 'Location: ./VUE/index.php');
-?>
